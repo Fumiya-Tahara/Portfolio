c3ec81f494cbb6458952e493bbb4956efa5b0b67
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-09-04 08:31:06 +0900

    add profile

---
+++ src/app/components/profile/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Profile() {
+  return (
+    <div className="bg-gray-300 py-24">
+      <div className="flex justify-center mb-10">
+        <span className="text-6xl">PROFILE</span>
+      </div>
+      <div className="flex justify-center">
+        <div className="w-2/3 bg-white rounded-lg flex justify-center items-center" style={{"aspectRatio": 1.618 / 1}}>
+          <div className="flex justify-center pb-20 w-2/3">
+            <div>
+              <a href="https://github.com/Fumiya-Tahara">
+                <Image src={"/icon/github-icon.svg"} alt="GitHub" width={100} height={100} />
+              </a>
+            </div>
+            <div className="w-2/3 ml-6">
+              <p>福岡県出身の学生エンジニア。大学では農学を学んでいる。プログラミングは大学入学の少し前に始めた。主にWeb開発に興味があり、現在はGoやTypeScriptを学んでいる。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
+import Top from "./components/top/page";
+import Profile from "./components/profile/page";
+import Skill from "./components/skill/page";
+import Highlight from "./components/highlight/page";
 
 
 export default function Home() {
   return (
     <>
+      <Top />
+      <Profile />
+      <Skill />
+      <Highlight />
     </>
   )
 }
