2d4b994d0dd5c09582ffd318a8fbd77b1587c6af
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-09-12 17:04:44 +0900

    highlight

--- src/app/components/highlight/components/record.tsx
+++ src/app/components/highlight/components/record.tsx
@@ -6,10 +6,10 @@ interface RecordProps {
 
 export function Record({date, title, description}: RecordProps) {
   return (
-    <div className="flex">
-      <dt>{date}</dt>
-      <dd className="border-l-4">
-        <div>{title}</div>
+    <div className="grid grid-cols-3">
+      <dt className="text-right">{date}</dt>
+      <dd className="border-l-4 col-span-2 pl-2.5 pb-5">
+        <div className="font-bold">{title}</div>
         <div>{description}</div>
       </dd>
     </div>
--- src/app/components/highlight/page.tsx
+++ src/app/components/highlight/page.tsx
@@ -8,18 +8,23 @@ export default function Highlight() {
         <span className="text-6xl">HIGHLIGHT</span>
       </div>
       <div className="flex justify-center">
-        <div className="w-2/3 bg-white rounded-lg">
-          <div>
-            <dl className="flex flex-col items-center">
+        <div className="w-2/3 bg-white rounded-lg flex justify-center py-10">
+          <div className="w-2/3">
+            <dl className="flex flex-col">
               <Record
                 date="2021年3月"
                 title="Hello World!"
                 description="Pythonで初めてプログラムを書いた"
               />
-                <Record
+              <Record
                 date="2022年4月"
                 title="初のハッカソンに参加"
                 description="技育CAMPのはじめてのハッカソンに参加したが何も貢献できなかった。"
+              />
+              <Record
+                date="2023年8月"
+                title="北九州ハッカソンに参加"
+                description="運営、開発ともに参加して開発チームでは優秀賞を受賞した。"
               />
             </dl>
           </div>
