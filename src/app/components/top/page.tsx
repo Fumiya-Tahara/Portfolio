f5d0899f50cd131ee14ddbc8afcc64f54327cde0
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-09-17 20:56:44 +0900

    responsive

--- src/app/components/top/page.tsx
+++ src/app/components/top/page.tsx
@@ -1,11 +1,11 @@
 export default function Top() {
   return (
-    <div className="h-screen pt-24 min-h-screen" style={{backgroundColor: "#2c6bb1"}}>
-      <div className="text-8xl text-white text-right ml-52 pr-4 pb-2 rounded-l-lg" style={{backgroundColor: "#004898", color: "#fff"}}>
+    <div className="h-screen min-h-screen pt-12 lg:pt-24" style={{backgroundColor: "#2c6bb1"}}>
+      <div className="text-white text-right text-4xl lg:text-8xl ml-16 lg:ml-52 pr-4 pb-2 rounded-l-lg" style={{backgroundColor: "#004898", color: "#fff"}}>
       {/* eslint-disable-next-line react/no-unescaped-entities */}
         <div>Fumiya Tahara's Portfolio</div>
-        <div className="text-2xl text-right">My Engineering Diary</div>
       </div>
+      <div className="text-right text-white text-base lg:text-2xl pr-4">My Engineering Diary</div>
     </div>
   )
 }
