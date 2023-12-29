e6dd18ba96b5a1f85dd14cf4761b739d6c6b7002
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-12-29 21:14:23 +0900

    add svg and image

--- src/app/components/top/page.tsx
+++ src/app/components/top/page.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
+
 export default function Top() {
   return (
-    <div className="h-screen min-h-screen pt-12 sm:pt-16 lg:pt-24" style={{backgroundColor: "#2c6bb1"}}>
-      <div className="text-white text-right text-4xl sm:text-6xl lg:text-8xl ml-16 sm:ml-32 lg:ml-52 pr-4 pb-2 rounded-l-lg" style={{backgroundColor: "#004898", color: "#fff"}}>
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-        <div>Fumiya Tahara's Portfolio</div>
+    <div className="h-screen bg-gray-800 grid grid-cols-6 gap-4">
+      <div className="col-start-2 col-span-4 flex justify-center mt-64">
+        <Image
+          src="/icon/barbell01.svg"
+          alt="barbell"
+          width={500}
+          height={500}
+        />
       </div>
-      <div className="text-right text-white text-base sm:text-xl lg:text-2xl pr-4">My Engineering Diary</div>
     </div>
-  )
+  );
 }
