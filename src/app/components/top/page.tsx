81661311fdfbfc31b4a1e56058177790ce489f07
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-12-30 15:24:33 +0900

    add animation

--- src/app/components/top/page.tsx
+++ src/app/components/top/page.tsx
@@ -1,9 +1,22 @@
+"use client";
 import Image from "next/image";
+import anime from "animejs/lib/anime.es.js";
+import { useEffect } from "react";
 
 export default function Top() {
+  useEffect(() => {
+    anime({
+      targets: ".barbell-img",
+      translateY: -200,
+      loop: 5,
+      direction: "alternate",
+      easing: "easeInOutSine",
+    });
+  }, []);
+
   return (
     <div className="h-screen bg-gray-800 grid grid-cols-6 gap-4">
-      <div className="col-start-2 col-span-4 flex justify-center mt-64">
+      <div className="barbell-img col-start-2 col-span-4 flex justify-center mt-96">
         <Image
           src="/icon/barbell01.svg"
           alt="barbell"
